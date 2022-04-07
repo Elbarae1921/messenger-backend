a34be497b496413226e70051d37503c23b3edebe
Elbarae Rguig <elbarae1921@gmail.com>
2022-04-07 15:14:28 +0000

    fix(cd): update CodeDeploy scripts

--- aws/after_install.sh
+++ aws/after_install.sh
@@ -2,4 +2,4 @@
 cd /home/elbarae/messenger-backend
 GITHUB_TOKEN=$(aws ssm get-parameters --names 'MESSENGER_BACKEND_GITHUB_TOKEN' --with-decryption --query 'Parameters[0].Value' --profile elbarae)
 echo $GITHUB_TOKEN | docker login ghcr.io -u elbarae1921 --password-stdin
-docker-compose up -d --build
+docker-compose --file docker-compose.prod.yml up -d --build
