f13a00a4679e9af63454373aae7f8d887b896000
Elbarae Rguig <elbarae1921@gmail.com>
2022-04-26 00:54:10 +0000

    fix(frontend): revert to react 18 to fix build errors

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
